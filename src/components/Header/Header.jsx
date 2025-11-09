e352f4f06817a16811f7bbc78fc5a2a6e9d6c252
mo1734 <01151540818moh@gmail.com>
2025-11-09 10:32:47 +0200

    updateTwo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -34,7 +34,7 @@ function Header() {
                         </motion.div>
 
                         <a 
-                            href="../../../public/Mohammed Ali Mohammed Hanafy5.pdf" 
+                            href="/Mohammed Ali Mohammed Hanafy5.pdf" 
                             download 
                             className="text-decoration-none custom-link-gold footer-link-size"
                         >
